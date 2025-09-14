397363630536093a001df9db520a7f97ef69ecde
kc7el <kc7el@DESKTOP-EROLTAG>
2025-09-13 20:14:02 -0500

    Agregar archivos de proyecto.

---
+++ ApiMatematicas/Controllers/ControladorMatematicas.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMatematicas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ControladorMatematicas : ControllerBase
+    {
+        private static Random rand = new Random();
+
+
+        [HttpGet("random")]
+        public IActionResult GetRandom()
+        {
+            int a = rand.Next(1, 10);
+            int b = rand.Next(1, 10);
+            return Ok(new { numero1 = a,numero2 = b });
+        }
+
+        [HttpPost ("pregunta")]
+        public IActionResult PostPregunta([FromBody] AnswerRequest request)
+        {
+            bool correcto =  request.respuestaUsuario == request.numero1 * request.numero2;
+            return Ok(new { correcto });
+        }
+    }
+
+    public class AnswerRequest
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+        public int respuestaUsuario { get; set; }
+    }
+
+}
