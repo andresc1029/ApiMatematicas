e01574f3a207b9e06e1d86cde56efe0eab3cf4ae
andresc1029 <kc7elpro@gmail.com>
2025-09-17 01:34:20 -0500

    Actu v3

--- ApiMatematicas/Controllers/ControladorMatematicas.cs
+++ ApiMatematicas/Controllers/ControladorMatematicas.cs
@@ -8,32 +8,72 @@ namespace ApiMatematicas.Controllers
     {
         private static Random rand = new Random();
         private static int contadorDeRacha = 0;
-
+        private static DateTime tiempoInicioPregunta = DateTime.MinValue;
+        private static bool? modoAnterior = null; 
 
         [HttpGet("random")]
         public IActionResult GetRandom()
         {
             int a = rand.Next(1, 10);
             int b = rand.Next(1, 10);
-            return Ok(new { numero1 = a,numero2 = b });
+            return Ok(new { numero1 = a, numero2 = b });
         }
 
-        [HttpPost ("pregunta")]
+        [HttpPost("pregunta")]
         public IActionResult PostPregunta([FromBody] AnswerRequest request)
         {
+            bool correcto = request.respuestaUsuario == request.numero1 * request.numero2;
 
-            bool correcto =  request.respuestaUsuario == request.numero1 * request.numero2;
-
-            if (correcto)
+            // reiniciar la racha
+            if (modoAnterior != null && modoAnterior != request.ModoConTiempo)
             {
-                contadorDeRacha++;
+                contadorDeRacha = 0;
+                tiempoInicioPregunta = DateTime.MinValue;
+            }
+            modoAnterior = request.ModoConTiempo;
+
+            if (request.ModoConTiempo)
+            {
+                if (tiempoInicioPregunta == DateTime.MinValue)
+                {
+                    tiempoInicioPregunta = DateTime.UtcNow;
+                }
+
+                var tiempoTranscurrido = DateTime.UtcNow - tiempoInicioPregunta;
+
+                int limiteTiempo = contadorDeRacha >= 15 ? 5 :  (contadorDeRacha >= 10 ? 10 : 15);
+                if (tiempoTranscurrido.TotalSeconds > limiteTiempo)
+                {
+                    contadorDeRacha = 0;
+                    tiempoInicioPregunta = DateTime.MinValue;
+                    return Ok(new { correcto = false, racha = contadorDeRacha, tiempoAgotado = true, tiempoInicio = limiteTiempo });
+                }
+
+                if (correcto)
+                {
+                    contadorDeRacha++;
+                    tiempoInicioPregunta = DateTime.UtcNow;
+                }
+                else
+                {
+                    contadorDeRacha = 0;
+                }
+                int tiempoRestante = Math.Max(0, limiteTiempo - (int)tiempoTranscurrido.TotalSeconds);
+
+                return Ok(new { correcto, racha = contadorDeRacha, tiempoUsuario = tiempoRestante, tiempoInicio = limiteTiempo });
             }
             else
             {
-                contadorDeRacha = 0;
-            }
+                if (correcto)
+                {
+                    contadorDeRacha++;
+                }
+                else
+                {
+                    contadorDeRacha = 0;
+                }
                 return Ok(new { correcto, racha = contadorDeRacha });
-
+            }
         }
     }
 
@@ -42,6 +82,6 @@ namespace ApiMatematicas.Controllers
         public int numero1 { get; set; }
         public int numero2 { get; set; }
         public int respuestaUsuario { get; set; }
+        public bool ModoConTiempo { get; set; }
     }
-
 }
